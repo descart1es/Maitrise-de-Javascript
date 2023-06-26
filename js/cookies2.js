a1c0e6416f2b5113a93223e864b4eed438d3ea3f
Descartes Bambu <descartesbambu@gmail.com>
2023-06-26 13:31:14 +0100

    new commit

--- js/cookies2.js
+++ js/cookies2.js
@@ -1,28 +1,37 @@
-let popup = document.getElementById("cookiePopup");
+let popUp = document.getElementById("cookiePopup");
 //Quand l'utilisateur click le bouton accepter
-document.getElementById('acceptcookie').addEventListener("click",() =>{
+document.getElementById('acceptcookie').addEventListener("click", () =>{
 // Ici on crée l'objet
-let d =new Date();
-d.setMinutes(2 +  d.getMinutes());
+let d = new Date();
+d.setMinutes( 2 +  d.getMinutes());
 
-document.cookie = "myCookieName=thisIsMyCookie;expires= "+ d +";";
-popup.classList.add("hide");
-popup.classList.remove("show");
+document.cookie = "myCookieName=thisIsMyCookie; expires = " + d +";";
+popUp.classList.add("hide");
+popUp.classList.remove("show");
+
 });
- const checkCookie = () =>{
+   const checkCookie = () =>{
     let input = document.cookie.split("=");
-    if (input[0]== "myCookieName"){
-        popup.classList.add("hide");
-        popup.classList.add("show");
-        
-    }else{
-        popup.classList.add("show");
-        popup.classList.add("hide");
+
+    if(input[0] == "myCookieName"){
+        popUp.classList.add("hide");
+        popUp.classList.remove("show");
+    }
+    else{
+        //popUp.classList.add("show");
+         popUp.classList.add('show');
+         popUp.classList.remove('hide');
+        //popUp.classList.remove("hide");
     }
  }
  //Ici on verifie si le cookie existe quand la page se charge
- window.onload = () =>{
-    setTimeout(() =>{
+ window.onload = () => {
+    setTimeout(()=>{
         checkCookie();
-    }, 2000)
+    },2000);
  }
+ 
+/* window.onload = () =>{
+    setTimeout(() => {
+        checkCookie();
+    },2000);*/
